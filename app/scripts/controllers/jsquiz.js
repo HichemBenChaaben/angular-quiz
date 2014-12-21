80b33afdbe3d3cb2217955d267cb1bf28e219f9c
HichemBenChaaben <hichem@dubizzle.com>
2014-12-21 23:13:19 +0400

    Removed counter

--- app/scripts/controllers/jsquiz.js
+++ app/scripts/controllers/jsquiz.js
@@ -26,31 +26,7 @@
         $scope.restart = false;
         $scope.points = 0;
         $scope.qtotal = 0;
-        $scope.counter = 5;
-        // pause the counter
-        $scope.isPaused = false;
-
-        // pause the counter
-        $scope.startCounter = function() {
-            $scope.isPaused = false;
-        };
-        // start the counter
-        $scope.pauseCounter = function() {
-            $scope.isPaused = true;
-        };
-        // function to set the time to answer to a question
-        var countDown = $interval(function() {
-            if($scope.counter > 0) {
-                // is paused means we need to pause the app for a while
-                if (!$scope.isPaused) {
-                    $scope.counter--;
-                }
-            } else {
-                $scope.counter = 5;
-            }
-        },1000);
-
-        countDown;
+        $scope.qindex = qindex;
 
         // when you click on a button answered became true.
         // it will became false later when the slide change
@@ -60,10 +36,10 @@
             $scope.progress = qindex / $scope.qtotal;
         };
 
+        // function to start the quiz app
         $scope.startQuiz = function () {
             getData();
             $scope.restart = 1;
-            console.log("button clicked");
         };
 
         // Function which return a promess
@@ -78,17 +54,20 @@
 
         // display the badass message
         $scope.displayCorrect = function(arg) {
+            // the animation happen
             $scope.correctAnswer = true;
-            $scope.usermessage = 'bravo!!!';
+            // we add points
             $scope.addpoints(arg);
+            // display progress
             $scope.displayProgress;
             // disable the button
             $scope.answered = true;
+            // we move to the next slide after 2 seconds
             $timeout(function() {
                 $scope.correctAnswer = false;
-                $scope.answered = false;
                 $scope.progress++;
-            }, 3000);
+                $scope.answered = false;
+            }, 2000);
         };
 
         // add points to the user
@@ -98,27 +77,37 @@
             $scope.points += arg;
         };
 
+        // display the correct answer to the user
+        $scope.correctTheUser = function() {
+
+        };
         // Handle is an answer is correct or wrong
         $scope.answer = function(arg, element) {
+            if ($scope.answered) {
+                return;
+            }
             if (arg === +$scope.questions[qindex].slide.answer) {
                 $scope.displayCorrect($scope.questions[qindex].slide.points);
                 $scope.usermessage = 'Correct';
             } else {
-                $scope.usermessage = 'Wrong';
+                $scope.usermessage = 'Wrong'; // for test issues
+                $scope.correctTheUser();
             }
+
             // check if not excelling the ttal
             if (qindex < $scope.qtotal - 1) {
                 $timeout(function() {
                     qindex++;
+                    $scope.qindex = qindex;
                     $scope.slides = $scope.questions[qindex];
                     hljs.initHighlightingOnLoad();
-                    $scope.usermessage.message = 'Correct';
                 }, 5000);
 
             } else {
                 // you finished the quiz questions
                 $scope.restart = true;
                 console.log('Move on to the next quiz');
+                // go to the stat state of the application
             }
         };
         $scope.resetQuiz = function() {
